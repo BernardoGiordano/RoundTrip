a1b764820b8847bcbc2e74b0d9d727723abe7cec
BernardoGiordano <bernardogiordano369@gmail.com>
2018-06-23 15:42:36 +0200

    Fix loop through locations

--- app/src/main/java/it/roundtrip/util/LocationSwitcher.java
+++ app/src/main/java/it/roundtrip/util/LocationSwitcher.java
@@ -2,9 +2,9 @@ package it.roundtrip.util;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.Timer;
@@ -12,14 +12,13 @@ import java.util.TimerTask;
 
 import it.roundtrip.dao.Dao;
 import it.roundtrip.pojo.Coordinate;
-import it.roundtrip.util.MyLocationManager;
-import it.roundtrip.util.MyRunnable;
 
 import static it.roundtrip.util.Constants.INDEX;
 import static it.roundtrip.util.Constants.LABEL;
 
 public class LocationSwitcher extends Service {
 
+    private boolean active = false;
     private Dao dao = Dao.getInstance();
 
     @Nullable
@@ -29,20 +28,28 @@ public class LocationSwitcher extends Service {
     }
 
     public int onStartCommand(Intent intent, int flags, int startId) {
-        final String cityKey = intent.getExtras().getString(LABEL);
-        final int startIndex = intent.getExtras().getInt(INDEX);
+        if (!active) {
+            final String cityKey = intent.getExtras().getString(LABEL);
+            final int startIndex = intent.getExtras().getInt(INDEX);
 
-        final int cityIndex = dao.cityIndex(cityKey);
-        final Handler handler = new Handler();
-        Timer t = new Timer();
-        t.schedule(new TimerTask() {
-            public void run() {
-                MyLocationManager mlc = new MyLocationManager(getApplicationContext());
-                final List<Coordinate> c = dao.getCoordinates();
-                handler.post(new MyRunnable(c, cityIndex, getApplicationContext()));
-            }
-        }, dao.getCities().get(cityIndex).getDelay(), dao.getCities().get(cityIndex).getCooldown() * 1000);
+            final int cityIndex = dao.cityIndex(cityKey);
+            Timer t = new Timer();
+            t.schedule(new TimerTask() {
+                private int i = startIndex;
+                public void run() {
+                    final List<Coordinate> coordinates = dao.getCoordinates();
+                    MyLocationManager mlc = new MyLocationManager(getApplicationContext());
+                    mlc.mockLocation(dao.getCities().get(cityIndex), coordinates.get(i));
+                    i = (i+1) % coordinates.size();
+                }
+            }, dao.getCities().get(cityIndex).getDelay(), dao.getCities().get(cityIndex).getCooldown() * 1000);
+
+            active = true;
+        } else {
+            Toast.makeText(getApplicationContext(), "Service already started, kill the app.", Toast.LENGTH_LONG).show();
+        }
 
         return Service.START_NOT_STICKY;
     }
+
 }
--- app/src/main/java/it/roundtrip/util/MyLocationManager.java
+++ app/src/main/java/it/roundtrip/util/MyLocationManager.java
@@ -1,9 +1,10 @@
 package it.roundtrip.util;
 
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.widget.Toast;
 
@@ -22,9 +23,6 @@ public class MyLocationManager {
 
     public void mockLocation(final City city, final Coordinate coordinate) {
         LocationManager lm = (android.location.LocationManager)context.getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-
         String mocLocationProvider = android.location.LocationManager.GPS_PROVIDER;
 
         lm.addTestProvider(mocLocationProvider, false, false,
@@ -42,7 +40,18 @@ public class MyLocationManager {
 
         // supposing Toast.LENGTH_SHORT is ~2000ms
         for (int i = 0; i < (city.getCooldown() * 1000) / 2000; i++) {
-            Toast.makeText(context, coordinate.getName() + "... " + Integer.toString(city.getCooldown() - 2*i) + "s", Toast.LENGTH_SHORT).show();
+            final int step = i;
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(
+                new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        Toast.makeText(context, coordinate.getName() + "... " + Integer.toString(city.getCooldown() - 2*step) + "s", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            );
         }
     }
 
